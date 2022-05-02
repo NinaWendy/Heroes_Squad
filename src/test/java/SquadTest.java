baf8842e745100f0c7147355189ddd307e10c1cb
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-02 16:30:17 +0300

    Run tests to Squad class behaviours.

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,3 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SquadTest {
+    @Test
+    void newSquad_instantiatesCorrectly() {
+        Squad squad= setUpNewSquad();
+        assertNotNull(squad);
+    }
+    @Test
+    public void getName_objectReturnsNameCorrectly_Thor() {
+        Squad squad = setUpNewSquad();
+        assertEquals("Avengers", squad.getSquadName());
+    }
+
+    @Test
+    public void update_objectCorrectlyUpdatesDetail() {
+        Squad squad = setUpNewSquad();
+        squad.update("X-men",4,"Corruption");
+        assertEquals("X-men", squad.getSquadName());
+    }
+
+    @Test
+    void delete_objectIsCorrectlyRemoved() {
+        ArrayList<Squad> instances = new ArrayList<>();
+        Squad squad = setUpNewSquad();
+        instances.add(squad);
+        Squad squad1 = setUpNewSquad();
+        instances.add(squad1);
+        instances.remove(1);
+        assertEquals(1,instances.size());
+    }
+
+
+    //Helper Method
+    public Squad setUpNewSquad(){
+        return new Squad("Avengers",5,"sexism");
+    }
 
 }
