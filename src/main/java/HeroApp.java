34bb893ff1d973026bc49b8c33cb2208c5fe50c0
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-02 15:48:12 +0300

    Add implementations.

--- src/main/java/HeroApp.java
+++ src/main/java/HeroApp.java
@@ -1,2 +1,32 @@
+import models.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeroApp {
+    private List<Hero> heroList = new ArrayList<>();
+
+    public List<Hero> getHeroList() {
+        return heroList;
+    }
+    public void addHero( List<Hero> list, Hero hero){
+        if(list != null){
+            heroList= list;
+        }
+        assert list != null;
+        hero.setId(list.size());
+        heroList.add(hero);
+    }
+
+    public  Hero findById(int id) {
+        return  heroList.get(id);
+    }
+
+    public void clearAllHeroes(){
+        heroList.clear();
+    }
+
+    public void deleteHero(int id) {
+        heroList.remove(id);
+    }
 }
